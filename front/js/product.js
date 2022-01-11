68b12d0a90371097cf193d5f50f3c1cd081f1d2a
Athéna Richard <richard.athena@gmail.com>
2022-01-11 22:36:27 +0100

    Fin de la page d'index et affichage dynamique de la page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,4 @@
+// Récupération de l'id du produit dans l'URL
 let adresse = window.location.href;
 console.log (adresse)
 
@@ -6,3 +7,50 @@ console.log(url)
 
 let reference = url.searchParams.get("id");
 console.log (reference)
+
+
+// Liaison entre l'id et les données dans l'API
+const api = `http://localhost:3000/api/products/${reference}`;
+console.log (api)
+
+fetch(api)
+    .then(function(reponse) {
+        return reponse.json();
+    })
+    .then(function(infosProduit) {
+        affichageProduit(infosProduit);
+        console.log (infosProduit);
+    })
+    .catch(function(erreur) {
+        console.log ("Erreur : " + erreur);
+    })
+
+
+// Création de la fonction qui permettra d'afficher les résultats dans le DOM
+function affichageProduit (infosProduit) {
+    // Ajout de l'image
+    let ajoutImage = document.getElementsByClassName("item__img")[0];
+    ajoutImage.innerHTML += `<img src="${infosProduit.imageUrl}" alt="${infosProduit.altTxt}">`;
+
+    // Ajout du titre
+    let ajoutTitre = document.getElementById("title");
+    ajoutTitre.insertAdjacentText ("afterbegin", `${infosProduit.name}`);
+
+    // Ajout du prix
+    let ajoutPrix = document.getElementById("price");
+    ajoutPrix.insertAdjacentText ("afterbegin", `${infosProduit.price}`);
+
+    // Ajout de la description
+    let ajoutDescription = document.getElementById("description");
+    ajoutDescription.insertAdjacentText ("afterbegin", `${infosProduit.description}`);
+
+    // Ajout des couleurs
+    let couleurs = [];
+    for (let j of infosProduit.colors) {
+        couleurs.push(j);
+    }
+    let ajoutCouleurs = document.getElementById("colors");
+    for (let k = 0; k < couleurs.length; k++) {
+        ajoutCouleurs.insertAdjacentHTML ("beforeend", `<option value="${couleurs[k]}">${couleurs[k]}</option>`);
+    }
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,65 @@
 const produits = document.getElementById("items");
 const url = "http://localhost:3000/api/products";
 
-// fetch(url)
+fetch(url)
+    .then(function(reponse) {
+        return reponse.json();
+    })
+    .then(function(data) {
+        creationProduits(data);
+        //console.log (data);
+    })
+    .catch(function(erreur) {
+        console.log ("Erreur : " + erreur);
+    })
+
+
+// Création de la class qui permettra de créer un modèle d'objet
+class CreationCanape {
+    constructor (_id, imageUrl, altTxt, name, description) {
+        this._id = _id;
+        this.imageUrl = imageUrl;
+        this.altTxt = altTxt;
+        this.name = name;
+        this.description = description;
+    }
+}
+
+// Création du tableau qui contiendra les objets créés
+let tabProduits = [];
+
+// Création de la fonction qui permettra de créer les objets et de les stocker dans le tableau
+function creationProduits(data) {
+   for (let canape of data) {
+       tabProduits.push(new CreationCanape(canape._id, canape.imageUrl, canape.altTxt, canape.name, canape.description));
+    }
+    affichageProduits();
+}
+
+// Création de la fonction qui permettra d'afficher les résultats dans l'index, à partir des objets stockés dans le tableau
+function affichageProduits () {
+    for (let i of tabProduits) {
+        // Utilisation de URLSearchParams pour le lien vers chaque produit
+        let adresse = "http://127.0.0.1:5500/front/html/product.html";
+        console.log(adresse)
+        let lien = new URL(adresse);
+        lien.searchParams.set("id", i._id)
+        console.log(lien)
+        
+        // Intégration dans le DOM
+        produits.innerHTML +=
+            `<a href="${lien}">
+                <article>
+                    <img src="${i.imageUrl}" alt="${i.altTxt}">
+                    <h3 class="productName">${i.name}</h3>
+                    <p class="productDescription">${i.description}</p>
+                </article>
+            </a>`;
+    }
+}
+
+// CODE FONCTIONNEL EN CAS D'ERREUR
+//  fetch(url)
 //     .then (function(reponse) {
 //         return reponse.json();
 //     })
@@ -21,64 +79,3 @@ const url = "http://localhost:3000/api/products";
 //     .catch (function(erreur) {
 //         console.log ("Erreur : " + erreur);
 //     })
-
-/*
-    Étapes =
-    1 - Créer la Class
-    2 - Créer le tableau qui contiendra les objets créés
-    3 - Créer la function a, qui créera les objets et les envoyer au tableau
-    4 - Créer la function b, qui permettra l'affichage des produits sur l'index, à partir des objets stockés dans le tableau
-    5 - Appeler la function b, depuis la fonction de création a
-*/
-
-fetch(url)
-    .then(function(reponse) {
-        return reponse.json();
-    })
-    .then(function(data) {
-        creationProduits(data);
-        console.log (data);
-    })
-    .catch(function(erreur) {
-        console.log ("Erreur : " + erreur);
-    })
-
-
-// Création de la class qui permettra de créer un modèle d'objet
-class CreationCanape {
-    constructor (_id, imageUrl, altTxt, name, description) {
-        this.id = _id;
-        this.image = imageUrl;
-        this.alt = altTxt;
-        this.name = name;
-        this.description = description;
-    }
-}
-
-// Création du tableau qui contiendra les objets créés
-let tabProduits = [];
-
-// Création de la fonction qui permettra de créer les objets et de les stocker dans le tableau
-function creationProduits(data) {
-
-   for (let canape of data) {
-       tabProduits.push(new CreationCanape(capane._id, canape.imageUrl, canape.altTxt, canape.name, canape.description));
-    }
-    affichageProduits();
-}
-
-// Création de la fonction qui permettra d'afficher les résultats dans l'index, à partir des objets stockés dans le tableau
-function affichageProduits () {
-
-    for (let i of tabProduits) {
-        produits.innerHTML +=
-            `<a href="./product.html?id=${i._id}">
-                <article>
-                    <img src="${i.imageUrl}" alt="${i.altTxt}"/>
-                    <h3 class="productName">${i.name}</h3>
-                    <p class="productDescription">${i.description}</p>
-                </article>
-            </a>`;
-    }
-}
-
